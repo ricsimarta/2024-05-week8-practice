c80f2d2a980c83b789ae28c21aa676579e25e0fd
ricsimarta <ricsimarta@gmail.com>
2024-07-05 20:09:21 +0200

    added fetch next button, and some styling

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 const characterComponent = (name, height, mass) => `
   <div class="character">
+    <h2> character: </h2>
     <p class="name"> ${name} </p>
     <p class="height"> ${height}cm </p>
     <p class="mass"> ${mass}kg </p>
+  </div>
+`;
+
+const charactersComponent = (charactersData) => `
+  <div class="characters">
+    ${charactersData
+      .map(characterData => characterComponent(characterData.name, characterData.height, characterData.mass))
+      .join(" ")
+    }
   </div>
 `;
 
@@ -13,9 +23,14 @@ async function fetchData() {
 
   const rootElement = document.querySelector("#root");
 
-  rootElement.insertAdjacentHTML("beforeend", characters
-    .map(character => characterComponent(character.name, character.height, character.mass))
-    .join(" "));
+  rootElement.insertAdjacentHTML("beforeend", charactersComponent(characters));
+  rootElement.insertAdjacentHTML("beforeend", '<button class="fetch">load more...</button>');
+
+  const fetchButtonElement = document.querySelector("button.fetch");
+  fetchButtonElement.addEventListener("click", () => {
+    console.log("fetch next page");
+    console.log(data.next);
+  })
 }
 
 fetchData();
