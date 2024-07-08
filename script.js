5f35ec5251251164653d809fd65b4876690e49aa
ricsimarta <ricsimarta@gmail.com>
2024-07-08 18:45:53 +0200

    added second page characters into dom

--- script.js
+++ script.js
@@ -10,26 +10,45 @@ const characterComponent = (name, height, mass) => `
 const charactersComponent = (charactersData) => `
   <div class="characters">
     ${charactersData
-      .map(characterData => characterComponent(characterData.name, characterData.height, characterData.mass))
-      .join(" ")
-    }
+    .map(characterData => characterComponent(characterData.name, characterData.height, characterData.mass))
+    .join(" ")
+  }
   </div>
 `;
+
+
+
 
 async function fetchData() {
   const fetchResult = await fetch("https://swapi.dev/api/people/");
   const data = await fetchResult.json();
-  const characters = data.results; 
+  const characters = data.results;
 
   const rootElement = document.querySelector("#root");
-
   rootElement.insertAdjacentHTML("beforeend", charactersComponent(characters));
   rootElement.insertAdjacentHTML("beforeend", '<button class="fetch">load more...</button>');
 
   const fetchButtonElement = document.querySelector("button.fetch");
-  fetchButtonElement.addEventListener("click", () => {
+  fetchButtonElement.addEventListener("click", async () => {
     console.log("fetch next page");
     console.log(data.next);
+
+    const newFetchResult = await fetch(data.next);
+    console.log(newFetchResult);
+    const newData = await newFetchResult.json();
+    console.log(newData);
+    const newCharacters = newData.results;
+    console.log(newCharacters);
+
+    rootElement.insertAdjacentHTML("beforeend", charactersComponent(newCharacters));
+
+    fetchButtonElement.remove();
+    rootElement.insertAdjacentHTML("beforeend", '<button class="fetch">load more...</button>');
+    const newFetchButtonElement = document.querySelector("button.fetch");
+    newFetchButtonElement.addEventListener("click", () => {
+      console.log("fetch third page");
+      console.log(newData.next);
+    })
   })
 }
 
